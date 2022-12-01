594e6f6ee7685b81da3c373374c66df1db3b9ad7
Layoolar <olaiwolaayoola@yahoo.com>
2022-12-01 21:02:48 +0100

    Edited main.py

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from msilib.schema import Class
-from secrets import choice
 
-echo "Hello World"
+print("Select player.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+
+choice = input("Choose a character ")
+
+
+
+print("Hello World")
